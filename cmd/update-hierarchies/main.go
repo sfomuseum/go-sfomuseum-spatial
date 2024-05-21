20c76954208930c5ce7d5315d3a16f2126d58aa3
sfomuseumbot <sfomuseumbot@localhost>
2024-05-20 17:38:33 -0700

    block out sfom-specific cmd/update-hierarchies tool

---
+++ cmd/update-hierarchies/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	_ "github.com/whosonfirst/go-whosonfirst-spatial-pmtiles"
+	"github.com/whosonfirst/go-whosonfirst-spatial/app/hierarchy/update"
+	"github.com/sfomuseum/go-sfomuseum-spatial/hierarchy"	
+)
+
+func main() {
+
+	ctx := context.Background()
+	logger := log.Default()
+
+	fs, err := update.DefaultFlagSet(ctx)
+
+	if err != nil {
+		log.Fatalf("Failed to create default flagset, %v", err)
+	}
+	
+	opts, err := update.RunOptionsFromFlagSet(ctx, fs)
+
+	if err != nil {
+		log.Fatalf("Failed to derive options from flagset, %v", err)
+	}
+
+	opts.SPRResultsFunc = hierarchy.ChoosePointInPolygonCandidate
+
+	err = update.RunWithOptions(ctx, opts, logger)
+
+	if err != nil {
+		log.Fatalf("Failed to run update hierarchy tool, %v", err)
+	}
+}
